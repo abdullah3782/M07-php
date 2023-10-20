e7f3993fba3a1a2d14dc4220d2ffac91568f3c58
Abdullah <abdullah.butt093@gmail.com>
2023-10-20 14:51:32 +0200

    asa

---
+++ formulario/inc/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>     
+   body {
+        background-image: url('/formulario/img/background.jpg'); /* Reemplaza 'ruta/de/tu/imagen.jpg' con la ruta real de tu imagen */
+        background-size: cover; /* Ajusta el tamaño de la imagen para cubrir todo el fondo */
+        background-repeat: no-repeat; /* Evita que la imagen se repita *//* Cambia la fuente si es necesario */
+    }</style>
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Subscribe</title>
+</head>
+<body>
+    <main>
