a1cfa9e15acd6a34315b6cef3b57c5ba63045b20
abdullah3782 <abdullah.butt093@gmail.com>
2023-12-13 10:12:00 +0100

    camvios

--- burgerProj/burgerProj/burgerProj/burger/inc/header.php
+++ burgerProj/burgerProj/burgerProj/burger/inc/header.php
@@ -8,7 +8,11 @@
     <title>Subscribe</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
+<style>
+    #descarga{
+        
+    }
+</style>
 </head>
 
 <body>
@@ -22,6 +26,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
+                    <img src="./img/descarga.png" alt="" id="descarga" style="float: right;">
                         <a href="./inc/login.php"><button class="nav-link" id="log" style="color: white;">Login</button></a>
                     </li>
 
