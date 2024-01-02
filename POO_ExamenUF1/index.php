7b22091cc582c85dbd8fedfeabceda0adb49c8e9
abdullah3782 <abdullah.butt093@gmail.com>
2024-01-02 18:18:04 +0100

    proyectoPOO

--- POO_ExamenUF1/index.php
+++ POO_ExamenUF1/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 class ACBProyecto
 {
@@ -13,6 +14,32 @@ class ACBProyecto
 
     public function mostrarClubes()
     {
+        // Incrementar el contador de visitas en cada carga de la página
+        $_SESSION['visitas'] = isset($_SESSION['visitas']) ? $_SESSION['visitas'] + 1 : 1;
+
+        echo "<p>Visitas: {$_SESSION['visitas']}</p>";
+
+        // Obtener el valor de la cookie 'estilo' si está configurada
+        $estilo = isset($_COOKIE['estilo']) ? $_COOKIE['estilo'] : '';
+
+        echo "<style>";
+        // Aplicar estilos según el valor de la cookie 'estilo'
+        switch ($estilo) {
+            case 'BARÇA':
+                echo "body { background-color: blue; font-size: 20px; }";
+                break;
+            case 'MADRID':
+                echo "body { background-color: white; font-size: 30px; }";
+                break;
+            case 'VALENCIA':
+                echo "body { background-color: orange; font-size: 40px; }";
+                break;
+            default:
+                // Estilo predeterminado si no hay cookie o el valor no coincide
+                echo "body { background-color: white; font-size: 16px; }";
+        }
+        echo "</style>";
+
         echo '<ul>';
         foreach ($this->clubes as $club) {
             $imagenPath = "images/logos_clubs/{$club}.png";
@@ -21,20 +48,17 @@ class ACBProyecto
         echo '</ul>';
     }
 
-    public function mostrarJugadores($club)
+    public function mostrarJugadores()
     {
-        echo "<h2>Jugadores de {$club}</h2>";
+        echo "<h2>Todos los jugadores</h2>";
 
-        $clubSeleccionado = trim($club);
+        echo '<table border="1">';
+        echo '<tr><th>Club</th><th>Nombre</th><th>Samarreta</th><th>Posición</th><th>Nacionalidad</th><th>Licencia</th><th>Altura</th><th>Edad</th><th>Temp.</th><th>Foto</th><th>Acción</th></tr>';
 
-        echo "<p>Club seleccionado: {$clubSeleccionado}</p>";
-
-        if (isset($this->jugadores[$clubSeleccionado])) {
-            echo '<table border="1">';
-            echo '<tr><th>Nombre</th><th>Samarreta</th><th>Posición</th><th>Nacionalidad</th><th>Licencia</th><th>Altura</th><th>Edad</th><th>Temp.</th><th>Foto</th></tr>';
-
-            foreach ($this->jugadores[$clubSeleccionado] as $jugador) {
+        foreach ($this->jugadores as $club => $jugadoresClub) {
+            foreach ($jugadoresClub as $jugador) {
                 echo '<tr>';
+                echo "<td>{$club}</td>";
                 echo "<td>{$jugador['NOM']}</td>";
                 echo "<td>{$jugador['SAMARRETA']}</td>";
                 echo "<td>{$jugador['POSICIÓN']}</td>";
@@ -44,39 +68,50 @@ class ACBProyecto
                 echo "<td>{$jugador['EDAD']}</td>";
                 echo "<td>{$jugador['TEMP.']}</td>";
                 echo "<td><img src='images/jugadores/{$jugador['FOTO']}' alt='{$jugador['NOM']}' width='50' height='50'></td>";
+                echo "<td><a href='index.php?verJugador=1&foto={$jugador['FOTO']}&nombre={$jugador['NOM']}'>Ver jugador</a></td>";
                 echo '</tr>';
             }
+        }
 
-            echo '</table>';
-        } else {
-            echo "<p>No se encontraron jugadores para el club {$clubSeleccionado}</p>";
+        echo '</table>';
+    }
+
+    public function mostrarInfoJugador()
+    {
+        if (isset($_GET['verJugador'])) {
+            $fotoJugador = $_GET['foto'];
+            $nombreJugador = $_GET['nombre'];
+
+            echo "<h2>Información del Jugador</h2>";
+            echo "<p>Nombre: {$nombreJugador}</p>";
+            echo "<img src='images/jugadores/{$fotoJugador}' alt='{$nombreJugador}' width='150' height='150'>";
         }
     }
 
     private function leerJugadores()
     {
         $jugadores = [];
-
+    
         // Ruta al archivo lligaACB.csv
         $archivoCSV = 'lligaACB.csv';
-
+    
         if (file_exists($archivoCSV)) {
             $datos = array_map('str_getcsv', file($archivoCSV));
-
+    
             // Obtener las cabeceras (nombres de las columnas)
             $cabeceras = array_shift($datos);
-
+    
             foreach ($datos as $fila) {
                 $jugador = array_combine($cabeceras, $fila);
                 $club = $jugador['CLUB'];
-
+    
                 // Agregar el jugador al club correspondiente
                 $jugadores[$club][] = $jugador;
             }
         } else {
             echo "<p>No se encontró el archivo CSV</p>";
         }
-
+    
         return $jugadores;
     }
 }
@@ -87,10 +122,12 @@ $proyectoACB = new ACBProyecto();
 // Mostrar los clubes
 $proyectoACB->mostrarClubes();
 
+// Mostrar la información del jugador si se ha proporcionado el parámetro 'verJugador' en la URL
+$proyectoACB->mostrarInfoJugador();
+
 // Mostrar los jugadores del club seleccionado si se ha proporcionado el parámetro 'club' en la URL
 if (isset($_GET['club'])) {
     $clubSeleccionado = $_GET['club'];
     $proyectoACB->mostrarJugadores($clubSeleccionado);
 }
-
 ?>
