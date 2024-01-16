0fc040d8b8140a38c02698b995cd688778710edc
abdullah3782 <abdullah.butt093@gmail.com>
2024-01-16 12:05:15 +0100

    mv2

---
+++ UF2/mvc1-juego-Jugadores/model/ModelInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+interface ControlModelInterface
+{
+
+    public function add($object);
+
+    public function modify($object);
+
+    public function delete($id);
+
+    public function searchById($id);
+
+    public function home();
+
+}
